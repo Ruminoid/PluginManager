d7b90156923406b61598c32e36f4094434593589
Il Harper <afanyiyu@hotmail.com>
2020-08-20 17:58:24 +0800

    feat: MainWindow DataContext Update

--- Windows/MainWindow.DataContext.cs
+++ Windows/MainWindow.DataContext.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ruminoid.PluginManager.Windows
 {
-    public partial class MainWindow
+    public partial class MainWindow : INotifyPropertyChanged
     {
+        #region Global Data
+
+        private string _dataSource = "";
+
+        public string DataSource
+        {
+            get => _dataSource;
+            set
+            {
+                _dataSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Property Changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
