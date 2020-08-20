0c0154197fe57420c122eff4a210f390e2e31122
Il Harper <afanyiyu@hotmail.com>
2020-08-20 19:18:11 +0800

    feat: Add VC++ Install Button

--- Windows/MainWindow.DataContext.cs
+++ Windows/MainWindow.DataContext.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Ruminoid.PluginManager.Windows
 {
@@ -36,5 +39,8 @@ namespace Ruminoid.PluginManager.Windows
         }
 
         #endregion
+
+        private void InstallVCButtonBase_OnClick(object sender, RoutedEventArgs e) =>
+            Process.Start(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Libraries/VC_redist.x64.exe"));
     }
 }
