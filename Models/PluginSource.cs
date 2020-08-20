161c85bb6a0eee69837e4b154d62e931b3981639
Il Harper <afanyiyu@hotmail.com>
2020-08-20 17:57:58 +0800

    feat: Add PluginSource Class

---
+++ Models/PluginSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.PluginManager.Models
+{
+    public sealed class PluginSource
+    {
+        #region Current
+
+        public static PluginSource CurrentWebSource { get; set }
+
+        public static PluginSource CurrentLocalSource { get; set }
+
+        #endregion
+    }
+}
